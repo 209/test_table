488b2e9b3d72fcaa5b0696c0a9e9e7f7f16260e9
Ilya <ilya@diem.health>
2020-06-30 16:30:59 +0300

    add styles

--- src/ui/Button/index.jsx
+++ src/ui/Button/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const buttonStyle = {
+  padding: '5px 10px 5px 10px',
+  margin: '5px 0 5px 0',
+};
+
 function Button(props) {
-  const { children, ...otherProps } = props;
+  const { children, style, ...otherProps } = props;
 
   return (
-    <button type="button" {...otherProps}>
+    <button style={{ ...buttonStyle, ...style }} type="button" {...otherProps}>
       {children}
     </button>
   );
 }
 Button.propTypes = {
   children: PropTypes.node.isRequired,
+  style: PropTypes.object,
+};
+Button.defaultProps = {
+  style: {},
 };
 
 export default Button;
